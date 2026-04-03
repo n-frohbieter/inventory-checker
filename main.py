3bce55f2e5f708a0b48856775c8dada1cc988a2f
Nathan Frohbieter <nfrohbieter@gmail.com>
2026-04-02 21:12:06 -0500

    updated main loop

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 inventory = {}
 
+# Main loop that runs the program
 while True:
+    print()
     print('Enter x to exit program.')
 
     user_input = input('Enter Choice: ')
+    print()
+
+    if user_input.lower() == 'x':
+        print('Exiting the program!')
+        break
+
+    else:
+        print('Invalid Input!')
+        continue
